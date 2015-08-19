a40b7b06b4c589114b000af15909362ec5684d18
dipendran <dipendra.neupane@gmail.com>
2015-08-19 20:34:46 +1000

    Added exception handler for app level

--- RiskApp/Client/Risk.Client/App.xaml.cs
+++ RiskApp/Client/Risk.Client/App.xaml.cs
@@ -13,5 +13,17 @@ namespace Risk.Client
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var ex = e.ExceptionObject as Exception;
+            MessageBox.Show(ex.Message, "Uncaught Thread Exception",MessageBoxButton.OK, MessageBoxImage.Error);
+        }
     }
 }
