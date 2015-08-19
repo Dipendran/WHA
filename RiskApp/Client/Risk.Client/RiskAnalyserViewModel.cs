d6e93ac5ac933547372908e5cdba74bd959a504e
dipendran <dipendra.neupane@gmail.com>
2015-08-19 21:48:48 +1000

    removed unused reference

--- RiskApp/Client/Risk.Client/RiskAnalyserViewModel.cs
+++ RiskApp/Client/Risk.Client/RiskAnalyserViewModel.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Practices.ObjectBuilder2;
 using Risk.Client.Command;
 using Risk.Model;
 using Risk.Service.Services;
